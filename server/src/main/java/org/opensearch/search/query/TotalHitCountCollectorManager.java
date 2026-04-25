rishabhmaurya/OpenSearch

commit bab2e59bb15a4e6e0a5c926f5ea2d7cc4318365b

Update to Lucene 9.3.0 (#4043)
Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

server/src/main/java/org/opensearch/search/query/TotalHitCountCollectorManager.java
@@ -8,7 +8,10 @@
 
 package org.opensearch.search.query;
 
+import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.CollectorManager;
+import org.apache.lucene.search.LeafCollector;
+import org.apache.lucene.search.Scorable;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.TopDocs;
@@ -33,7 +36,15 @@ public class TotalHitCountCollectorManager
 
     private static final TotalHitCountCollector EMPTY_COLLECTOR = new TotalHitCountCollector() {
         @Override
-        public void collect(int doc) {}
+        public LeafCollector getLeafCollector(LeafReaderContext context) throws IOException {
+            return new LeafCollector() {
+                @Override
+                public void setScorer(Scorable scorer) throws IOException {}
+
+                @Override
+                public void collect(int doc) throws IOException {}
+            };
+        }
 
         @Override
         public ScoreMode scoreMode() {

buildSrc/version.properties
@@ -1,5 +1,5 @@
 opensearch        = 3.0.0
-lucene            = 9.3.0-snapshot-b7231bb
+lucene            = 9.3.0
 
 bundled_jdk_vendor = adoptium
 bundled_jdk = 17.0.3+7

modules/lang-expression/licenses/lucene-expressions-9.3.0-snapshot-b7231bb.jar.sha1
@@ -1,1 +1,0 @@
-57ae445a0050ad492ef494b692b486dfe718b564

modules/lang-expression/licenses/lucene-expressions-9.3.0.jar.sha1
@@ -1,0 +1,1 @@
+5583bcd3a24d3aae40b0a3152458021844ac09aa

plugins/analysis-icu/licenses/lucene-analysis-icu-9.3.0-snapshot-b7231bb.jar.sha1
@@ -1,1 +1,0 @@
-b10e5bdae6df879b770060e0006bbc1c780c886d

plugins/analysis-icu/licenses/lucene-analysis-icu-9.3.0.jar.sha1
@@ -1,0 +1,1 @@
+11dd9be0448fe594cf918f5260e193b3ab4e07a0

plugins/analysis-kuromoji/licenses/lucene-analysis-kuromoji-9.3.0-snapshot-b7231bb.jar.sha1
@@ -1,1 +1,0 @@
-f0ddc3072fd16012dafc74928f87fdfd7669ea4a

plugins/analysis-kuromoji/licenses/lucene-analysis-kuromoji-9.3.0.jar.sha1
@@ -1,0 +1,1 @@
+87c1357612f2f483174d1a63ea8c6680a1696bac

commit 3d344b27e7ea0ddc9e367107d6c52519eec1b524

Fix memory leak: static EMPTY_COLLECTOR captures query-specific Weight via setWeight
TotalHitCountCollectorManager.EMPTY_COLLECTOR is a static singleton reused
across queries. Since Lucene 9.3.0, ContextIndexSearcher.searchLeaf() calls
collector.setWeight(wrapWeight(weight)) on all collectors. The wrapped Weight
captures the ContextIndexSearcher (via anonymous inner class this reference),
which holds the DefaultSearchContext and all its aggregation results.

This pins the last query's entire search context (including aggregation
results like StringTerms buckets and HLL structures) permanently in heap,
causing progressive heap bloat on data nodes running size=0 aggregation
queries.

Fix: override setWeight as a no-op on the static singleton to prevent it
from capturing query-specific state.

Bug introduced in: #4043 (Lucene 9.3.0 upgrade, Aug 2022)
Singleton introduced in: #1500 (Concurrent Searching, Mar 2022)

server/src/main/java/org/opensearch/search/query/TotalHitCountCollectorManager.java
@@ -18,6 +18,7 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TopFieldDocs;
 import org.apache.lucene.search.TotalHitCountCollector;
 import org.apache.lucene.search.TotalHits;
+import org.apache.lucene.search.Weight;
 import org.opensearch.common.lucene.Lucene;
 import org.opensearch.common.lucene.search.TopDocsAndMaxScore;
 
@@ -44,6 +45,11 @@ public class TotalHitCountCollectorManager
                 @Override
                 public void collect(int doc) throws IOException {}
             };
+        }
+
+        @Override
+        public void setWeight(Weight weight) {
+            // no-op: static singleton must not capture query-specific state
         }
 
         @Override